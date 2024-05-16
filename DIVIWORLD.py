f7910c12a4ebe7517c340bcd705294b7912df301
Muntaha Chowdhury <muntaha06@gmail.com>
2024-05-16 16:57:31 +1000

    Inital Commit

---
+++ DIVIWORLD.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from tkinter import *
+
+root = Tk()
+root.title("DiviWorld")
+
+root.geometry("600x600")
+
+
+
+welcome_label = Label(root, text="Welcome to DiviWorld!")
+welcome_label.grid(row=1, column=0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mainloop()

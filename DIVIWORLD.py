a0a045cf1633d77a5e73163ce63d774561a6b27e
Muntaha Chowdhury <muntaha06@gmail.com>
2024-05-17 18:16:57 +1000

    Adjusted window and font sizes

--- DIVIWORLD.py
+++ DIVIWORLD.py
@@ -1,34 +1,35 @@
 # -*- coding: utf-8 -*-
 
-##########################
-#This comment is written to check whether git version control is working properly
-##########################
 
-
+import tkinter as tk
 from tkinter import *
+import tkinter.font as tkFont
+
+
+
 
 root = Tk()
 root.title("DiviWorld")
 
-root.geometry("600x600")
 
 
+# creating a font object
+fontobj = tkFont.Font(family="Comic Sans MS", size=30, weight="bold")
 
-welcome_label = Label(root, text="Welcome to DiviWorld!")
-welcome_label.grid(row=1, column=0)
-
-
-
-
-
-
-
+# the messages on the welcome screen
+welcome_label_1 = Label(root, text="Welcome to DiviWorld!", font=fontobj).grid(row=5, column=1, sticky="WE")
+welcome_label_2 = Label(root, text="Ready to amaze the world with your division skills?", font=fontobj).grid(row=7, column=1, sticky="WE")
+welcome_label_3 = Label(root, text="Click on the Bear to view instructions!", font=fontobj).grid(row=9, column=1, sticky="WE")
 
 
 
 
 
 
+root.grid_columnconfigure(0, weight=1) 
+
+
+root.geometry("1000x700")
 
 
 
@@ -37,4 +38,11 @@ welcome_label.grid(row=1, column=0)
 
 
 
-mainloop()
+
+
+
+
+
+
+
+root.mainloop()
